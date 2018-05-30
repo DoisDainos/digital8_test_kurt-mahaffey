524002698dc3e7c52d5496e1698fefdb6e3b1379
Kurt Mahaffey <kurtmahaf@hotmail.com>
2018-05-30 12:29:53 +1000

    Set up HashRouter and created main page.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import {Route, HashRouter} from 'react-router-dom';
 import './App.css';
+import Main from './Main';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Digital8 test</h1>
-        </header>
-        <p className="App-intro">
-        </p>
-      </div>
+      <HashRouter>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Digital8 test</h1>
+          </header>
+          <div className="App-content">
+            <Route exact path="/" component={Main}/>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
