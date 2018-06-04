ef609b0153db73f300a558f1bfb10a7c2980d044
Kurt Mahaffey <kurtmahaf@hotmail.com>
2018-06-04 16:40:46 +1000

    Score display, user can request the name of the product as a hint but will lose score.

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import APICalls from './APICalls';
 import Button from '@material-ui/core/Button';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 /*
  * Contains buttons that interact with API.
@@ -14,7 +19,8 @@ class Game extends Component {
       products: [],
       rounds: 3,
       score: 0,
-      progress: 0
+      progress: 0,
+      hintOpen: false
     }
   }
 
@@ -24,6 +30,11 @@ class Game extends Component {
   addProductImageToFrame(product) {
     var image = document.createElement("IMG");
     var imageDiv = document.getElementById("frame");
+    var hintBox = document.getElementById("hint-box");
+    if (hintBox.childNodes[1]) {
+      hintBox.removeChild(hintBox.childNodes[1]);
+    }
+    document.getElementById("hint-button").style.display="inline-block";
     while (imageDiv.firstChild) {
       imageDiv.removeChild(imageDiv.firstChild);
     }
@@ -54,11 +65,12 @@ class Game extends Component {
    */
   gameEnd() {
     var frame = document.getElementById("frame");
+    var winDisplay = document.createElement("H4");
+    document.getElementById("hint-button").style.display="none";
     while (frame.firstChild) {
       frame.removeChild(frame.firstChild);
     }
     frame.appendChild(document.createTextNode("GAME COMPLETE"));
-    var winDisplay = document.createElement("H4");
     winDisplay.innerHTML = "Your score: " + this.state.score;
     frame.appendChild(winDisplay);
     console.log("You win!");
@@ -85,9 +97,6 @@ class Game extends Component {
     } else {
       document.getElementById("input").value = "";
       this.setState({score: this.state.score - 20});
-      if (this.state.score <= 0) {
-        this.setState({score: 0});
-      }
       console.log("wrong");
     }
   }
@@ -143,6 +152,8 @@ class Game extends Component {
   startGame() {
     document.getElementById("game-intro").style.display = "none";
     document.getElementById("start-game").style.display = "none";
+    document.getElementById("hint-button").style.display = "inline-block";
+    document.getElementById("score").style.display = "inline-block";
     this.shuffleProductsArray();
     this.playRound(0);
   }
@@ -151,22 +162,67 @@ class Game extends Component {
     var chosenCategory = this.props.location.pathname.split("/")[3];
     document.getElementById("game-heading").innerHTML = "Category: " +
     chosenCategory;
-    this.setState({category: chosenCategory});var token = this.props.location.pathname.split("/")[2];
+    document.getElementById("hint-button").style.display = "none";
+    document.getElementById("score").style.display = "none";
+    this.setState({category: chosenCategory});
+    var token = this.props.location.pathname.split("/")[2];
     APICalls.getCategoryProducts(token, this, chosenCategory);
+  }
+
+  toggleHint() {
+    if (!this.state.hintOpen) {
+      this.setState({hintOpen: true});
+    } else {
+      this.setState({hintOpen: false});
+    }
+  }
+
+  showHint() {
+    var hint;
+    this.toggleHint();
+    this.setState({score: this.state.score - 20});
+    document.getElementById("hint-button").style.display="none";
+    hint = document.createElement("P");
+    hint.innerHTML = this.state.products[this.state.progress/34]["name"];
+    document.getElementById("hint-box").appendChild(hint);
   }
 
   render() {
     return (
       <div>
         <h3 id="game-heading">Game page</h3>
-        <p id="game-intro">Guess the price of the items</p>
-        <LinearProgress variant="determinate" value={this.state.progress} />
-        <br />
-        <Button id="start-game" variant="raised" color="primary"
-        onClick={() => this.startGame()}>
-          Start game
-        </Button>
-        <div id="frame"/>
+        <h3 id="score">Score: {this.state.score}</h3>
+        <div id="round-display">
+          <p id="game-intro">Guess the price of the items</p>
+          <LinearProgress variant="determinate" value={this.state.progress} />
+          <br />
+          <Button id="start-game" variant="raised" color="primary"
+          onClick={() => this.startGame()}>
+            Start game
+          </Button>
+          <Dialog
+            open={this.state.hintOpen}
+          >
+            <div className="dialog-info">
+              <h3>See product name?</h3>
+              <p>You will lose 20 points.</p>
+            </div>
+            <DialogActions>
+              <Button onClick={() => this.toggleHint()} color="primary">
+                No
+              </Button>
+              <Button onClick={() => this.showHint()} color="primary" autoFocus>
+                Yes
+              </Button>
+            </DialogActions>
+          </Dialog>
+          <div id="hint-box">
+            <Button id="hint-button" onClick={() => this.toggleHint()}>
+              See name
+            </Button>
+          </div>
+          <div id="frame"/>
+        </div>
       </div>
     );
   }
